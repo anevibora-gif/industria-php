c67ffb873fdf6240b034e69f4a5291e61bd6975f
Vivian Ariane <anevibora@gmail.com>
2026-06-25 11:05:41 -0300

    Classe Produto sendo implementada

---
+++ Produto.php
@@ -0,0 +1,47 @@
+<?php
+
+class Produto
+{
+    private $codigo;
+    private $nome;
+    private $preco;
+
+    public function __construct($codigo, $nome, $preco)
+    {
+        $this->codigo = $codigo;
+        $this->nome = $nome;
+        $this->preco = $preco;
+    }
+
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+}
+
+?>
